0f87c639831d289bcfc71c4d388835590612d494
shengkai <maimemdada@gmail.com>
2024-07-20 14:23:31 +0700

    Init project

---
+++ src/test/java/com/demo/mycoolapp/MyCoolAppApplicationTests.java
@@ -0,0 +1,14 @@
+package com.demo.mycoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class MyCoolAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
